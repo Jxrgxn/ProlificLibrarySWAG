a1b2cbbe3188dcc6a0465e7cc8866ab60b38a943
KacheMoney <basel.farag@gmail.com>
2014-10-24 13:36:47 -0500

    first commit

---
+++ ProlificLibrarySWAG/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProlificLibrarySWAG
+//
+//  Created by Basel Farag on 10/24/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ProlificLibrarySWAGTests/ProlificLibrarySWAGTests.m
@@ -0,0 +1,34 @@
+//
+//  ProlificLibrarySWAGTests.m
+//  ProlificLibrarySWAGTests
+//
+//  Created by Basel Farag on 10/24/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProlificLibrarySWAGTests : XCTestCase
+
+@end
+
+@implementation ProlificLibrarySWAGTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
