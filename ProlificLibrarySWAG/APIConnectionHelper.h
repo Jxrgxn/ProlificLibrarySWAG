5bd716fb0c30a6184515473daf6d4da247775a65
KacheMoney <basel.farag@gmail.com>
2014-10-29 02:01:20 -0500

    Begin Building APIHandler to control AFNetworking requests

---
+++ ProlificLibrarySWAG/APIConnectionHelper.h
@@ -0,0 +1,13 @@
+//
+//  APIConnectionHelper.h
+//  ProlificLibrarySWAG
+//
+//  Created by Basel Farag on 10/29/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIConnectionHelper : NSObject
+
+@end
---
+++ ProlificLibrarySWAG/APIConnectionHelper.m
@@ -0,0 +1,13 @@
+//
+//  APIConnectionHelper.m
+//  ProlificLibrarySWAG
+//
+//  Created by Basel Farag on 10/29/14.
+//  Copyright (c) 2014 ABaselNotBasilProduction. All rights reserved.
+//
+
+#import "APIConnectionHelper.h"
+
+@implementation APIConnectionHelper
+
+@end
